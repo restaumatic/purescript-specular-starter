4d123ab4ca877012ad53c8ae755b51aba1651f52
Michal Kozakiewicz <morswin@gmail.com>
2021-05-05 18:06:39 +0200

    Fix to work with latest specular

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,6 @@ module Main where
 import Prelude
 import Effect (Effect)
 
-import Data.Functor.Contravariant (cmap)
-
-import Specular.Callback (mkCallback, triggerCallback)
 import Specular.Dom.Browser ((:=))
 import Specular.Dom.Element (attrs, class_,  el,  onClick_, text, dynText)
 import Specular.Dom.Widget (runMainWidgetInBody)
@@ -19,12 +16,11 @@ main = do
   -- | Will append widget to the body
   runMainWidgetInBody do
     counter :: Ref Int <- Ref.new 0
-  
-    -- | Subtract 1 from counter value the straight forward way
-    let subtractCb = mkCallback \_ -> triggerCallback (Ref.modify counter) (add (negate 1))
+    -- | Subtract 1 from counter value
+    let subtractCb = Ref.modify counter (add (negate 1))
 
-    -- | Add 1 to counter value using the contravariant instance
-    let addCb = cmap (\_ -> add 1) (Ref.modify counter)
+    -- | Add 1 to counter value
+    let addCb = Ref.modify counter (add 1)
 
     el "button" [class_ "btn", attrs ("type":="button" ), onClick_ addCb ] do
       text "+"
