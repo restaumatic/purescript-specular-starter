496660b66637a1abf1cf5c0b15412fa35b1798f6
Michal Kozakiewicz <morswin@gmail.com>
2021-05-05 18:08:16 +0200

    Better imports

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,9 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
 
-import Specular.Dom.Browser ((:=))
-import Specular.Dom.Element (attrs, class_,  el,  onClick_, text, dynText)
+import Effect (Effect)
+import Specular.Dom.Element (attr,  class_, dynText, el, onClick_, text)
 import Specular.Dom.Widget (runMainWidgetInBody)
 import Specular.Ref (Ref)
 import Specular.Ref as Ref
@@ -22,10 +21,10 @@ main = do
     -- | Add 1 to counter value
     let addCb = Ref.modify counter (add 1)
 
-    el "button" [class_ "btn", attrs ("type":="button" ), onClick_ addCb ] do
+    el "button" [class_ "btn", attr "type" "button", onClick_ addCb ] do
       text "+"
 
     dynText $ show <$> Ref.value counter
 
-    el "button" [class_ "btn", attrs ("type":="button" ), onClick_ subtractCb ] do
+    el "button" [class_ "btn", attr "type" "button", onClick_ subtractCb ] do
       text "-"
